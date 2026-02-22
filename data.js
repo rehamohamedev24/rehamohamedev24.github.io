e197f8b67f9d645e1a346660421af06a959a1a90
Reham Mohammed <rehamohamedev24@gmail.com>
2026-02-22 20:28:09 +0200

    Update data.js

--- data.js
+++ data.js
@@ -11,8 +11,7 @@ window.PORTFOLIO = {
 
 
   summary:
-    "Crafting High-Quality Mobile Solutions with Clean Code & Modern Architecture.",
-
+"Android Developer specializing in building scalable, high-performance mobile applications using Kotlin, MVVM, and Clean Architecture. Passionate about crafting high-quality mobile solutions with clean, maintainable code and modern architecture principles."
   highlights: [
     "Kotlin",
     "MVVM",
